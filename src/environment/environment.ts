75b8707d0137e7f5ab1aec4ca90c05f30555f3c4
ShockStan <aaaeroa@gmail.com>
2024-04-18 23:23:36 +0530

    Resume and Skills Update

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,142 +1,162 @@
 export const socialMedia = {
-    twitter:'https://twitter.com/overbowlershead',
-    instagram:'https://instagram.com/arvindh_adiya?igshid=ZDdkNTZiNTM=',
-    linkedin:'https://www.linkedin.com/in/arvindhKM',
-    github:'https://github.com/ShockStan',
-    email:'aaaeroa@gmail.com'
-}
+  twitter: 'https://twitter.com/overbowlershead',
+  instagram: 'https://instagram.com/arvindh_adiya?igshid=ZDdkNTZiNTM=',
+  linkedin: 'https://www.linkedin.com/in/arvindhKM',
+  github: 'https://github.com/ShockStan',
+  email: 'aaaeroa@gmail.com',
+};
 export const gitHub = {
-    flight:'https://github.com/ShockStan/Flight-Tkt-Booking-Angular',
-    email:'https://github.com/ShockStan/SendMailGmailAPINodeJS',
-    cricket:'https://github.com/ShockStan/cricket-api-v1',
-    portfolio:'https://github.com/ShockStan/portfolio',
-    spring:'https://github.com/ShockStan/flight-tkt-booking-api',
-    thymeleaf:'https://github.com/ShockStan/springboot-thymeleaf-posgresql-PBKDF2',
-    python:'https://github.com/ShockStan/Django-OnlineMarkerPlace-SportsEquipment',
-    swing:'https://github.com/ShockStan/TicTacToe-Bingo-JavaSwing',
-    c:'https://github.com/ShockStan/Find-replace-a-word-in-all-files-in-a-directory',
-    demo:'https://github.com/ShockStan/company-website-demo',
-    google:'https://github.com/ShockStan/NestJS_Kafka_GoogleAuth',
-    chatbot:'https://github.com/ShockStan/chatbot'
-}
+  flight: 'https://github.com/ShockStan/Flight-Tkt-Booking-Angular',
+  email: 'https://github.com/ShockStan/SendMailGmailAPINodeJS',
+  cricket: 'https://github.com/ShockStan/cricket-api-v1',
+  portfolio: 'https://github.com/ShockStan/portfolio',
+  spring: 'https://github.com/ShockStan/flight-tkt-booking-api',
+  thymeleaf:
+    'https://github.com/ShockStan/springboot-thymeleaf-posgresql-PBKDF2',
+  python:
+    'https://github.com/ShockStan/Django-OnlineMarkerPlace-SportsEquipment',
+  swing: 'https://github.com/ShockStan/TicTacToe-Bingo-JavaSwing',
+  c: 'https://github.com/ShockStan/Find-replace-a-word-in-all-files-in-a-directory',
+  demo: 'https://github.com/ShockStan/company-website-demo',
+  google: 'https://github.com/ShockStan/NestJS_Kafka_GoogleAuth',
+  chatbot: 'https://github.com/ShockStan/chatbot',
+};
 export const youTube = {
-    flight:'https://www.youtube.com/watch?v=yyW23LwwSnQ',
-    cricket:'https://www.youtube.com/watch?v=OEIqVBXdbTY',
-    portfolio:'',
-    thymeleaf:'https://www.youtube.com/watch?v=6wE-SZE0eT8',
-    python:'https://www.youtube.com/watch?v=Bz2XkhYT4bk',
-    swing:'https://www.youtube.com/watch?v=GJ2tBSXKiGQ',
-    demo:'https://youtu.be/aTZy5L6QtVU'
-}
+  flight: 'https://www.youtube.com/watch?v=yyW23LwwSnQ',
+  cricket: 'https://www.youtube.com/watch?v=OEIqVBXdbTY',
+  portfolio: '',
+  thymeleaf: 'https://www.youtube.com/watch?v=6wE-SZE0eT8',
+  python: 'https://www.youtube.com/watch?v=Bz2XkhYT4bk',
+  swing: 'https://www.youtube.com/watch?v=GJ2tBSXKiGQ',
+  demo: 'https://youtu.be/aTZy5L6QtVU',
+};
 export const links = {
-    flight:'https://tripsterflight.netlify.app',
-    cricket:'',
-    portfolio:'https://shockstan-arvindh.netlify.app',
-    spring:'https://flightticket-6dst.onrender.com/flights',
-    chatbot:'https://chatbot.shockstan.vercel.app/'
-}
-export const projects=[
-    {
-    name:'Ticket Booking App',
-    tech:[' Angular ', ' Java ', ' SpringBoot ', ' MongoDB ', ' TailwindCSS '],
-    caption:'Angular Ticket Booking',
-    desc:'Angular Ticket Booking App with Spring Boot API. You can select seats, book tickets and download them.',
-    link:links.flight,
-    github:gitHub.flight,
-    youtube:youTube.flight,
-    image:'flight.svg'
-  },{
-    name:'Google Auth - Kafka',
-    tech:[' NestJS ', ' NodeJS ', ' ApacheKafka ', ' PostgreSQL ', ' Docker '],
-    caption:'Google Auth in NestJS',
-    desc:'Google Authentication using PassportJS and Apache Kafka',
-    github:gitHub.google,
-    image:'google.svg'
-  },{
-    name:'AI Chat bot using PaLM',
-    tech:[' Angular ', ' PaLM_API ', ' TailwindCSS '],
-    caption:'AI Chatbot - PaLM API',
-    desc:'Google Bard Clone',
-    link:links.chatbot,
-    github:gitHub.chatbot,
-    image:'chatbot.svg'
-  },{
-    name:'Automatic Email Reply',
-    tech:['NodeJS','GoogleAPI'],
-    caption:'Program to send automatic replies to received mails',
+  flight: 'https://tripsterflight.netlify.app',
+  cricket: '',
+  portfolio: 'https://shockstan-arvindh.netlify.app',
+  spring: 'https://flightticket-6dst.onrender.com/flights',
+  chatbot: 'https://chatbot.shockstan.vercel.app/',
+};
+export const projects = [
+  {
+    name: 'Ticket Booking App',
+    tech: [' Angular ', ' Java ', ' SpringBoot ', ' MongoDB ', ' TailwindCSS '],
+    caption: 'Angular Ticket Booking',
+    desc: 'Angular Ticket Booking App with Spring Boot API. You can select seats, book tickets and download them.',
+    link: links.flight,
+    github: gitHub.flight,
+    youtube: youTube.flight,
+    image: 'flight.svg',
+  },
+  {
+    name: 'Google Auth - Kafka',
+    tech: [' NestJS ', ' NodeJS ', ' ApacheKafka ', ' PostgreSQL ', ' Docker '],
+    caption: 'Google Auth in NestJS',
+    desc: 'Google Authentication using PassportJS and Apache Kafka',
+    github: gitHub.google,
+    image: 'google.svg',
+  },
+  {
+    name: 'AI Chat bot using PaLM',
+    tech: [' Angular ', ' PaLM_API ', ' TailwindCSS '],
+    caption: 'AI Chatbot - PaLM API',
+    desc: 'Google Bard Clone',
+    link: links.chatbot,
+    github: gitHub.chatbot,
+    image: 'chatbot.svg',
+  },
+  {
+    name: 'Automatic Email Reply',
+    tech: ['NodeJS', 'GoogleAPI'],
+    caption: 'Program to send automatic replies to received mails',
     desc: 'NodeJS app made with Google Gmail API, where reply is sent to every new eamil received in inbox and then added to separate label',
-    github:gitHub.email,
-    image:'email.svg'
-  },{
-    name:'Review App',
-    tech:['React', 'Java', 'SpringBoot', 'BootStrap', 'MongoDB'],
-    caption:'Carousal display showcase in React',
-    desc:'Full Stack App built React and Spring Boot. You can see my favorite cricket players stats and watch them play, and also give rating to them.',
-    link:links.cricket,
+    github: gitHub.email,
+    image: 'email.svg',
+  },
+  {
+    name: 'Review App',
+    tech: ['React', 'Java', 'SpringBoot', 'BootStrap', 'MongoDB'],
+    caption: 'Carousal display showcase in React',
+    desc: 'Full Stack App built React and Spring Boot. You can see my favorite cricket players stats and watch them play, and also give rating to them.',
+    link: links.cricket,
     github: gitHub.cricket,
-    youtube:youTube.cricket,
-    image:'cricket.svg'
-  },{
-    name:'Portfolio',
-    tech:['Angular','TailwindCSS'],
-    caption:'Showcase my best',
-    desc:'You get to see my protfolio and check out projects I have built, skills I know and lot more. And this is that.',
-    link:links.portfolio,
+    youtube: youTube.cricket,
+    image: 'cricket.svg',
+  },
+  {
+    name: 'Portfolio',
+    tech: ['Angular', 'TailwindCSS'],
+    caption: 'Showcase my best',
+    desc: 'You get to see my protfolio and check out projects I have built, skills I know and lot more. And this is that.',
+    link: links.portfolio,
     github: gitHub.portfolio,
-    youtube:youTube.portfolio,
-    image:'portfolio.svg'
-  },{
-    name:'Rest API',
-    tech:['Java', 'SpringBoot', 'MongoDB'],
-    caption:'Spring Boot API',
-    desc:'Rest API built using Spring Boot with MongoDB. You perform GET, POST, PATCH and DELETE requests.',
-    link:links.spring,
+    youtube: youTube.portfolio,
+    image: 'portfolio.svg',
+  },
+  {
+    name: 'Rest API',
+    tech: ['Java', 'SpringBoot', 'MongoDB'],
+    caption: 'Spring Boot API',
+    desc: 'Rest API built using Spring Boot with MongoDB. You perform GET, POST, PATCH and DELETE requests.',
+    link: links.spring,
     github: gitHub.spring,
-    image:'spring.svg'
-  },{
-    name:'User Management',
-    tech:['Java', 'SpringBoot', 'Thymeleaf', 'PostgreSQL','TailwindCSS'],
-    caption:'Web App with Thymeleaf UI',
-    desc:'Spring Boot app with UI built using Thymeleaf templates. PBKDF2 password encryption is used for authentication.',
+    image: 'spring.svg',
+  },
+  {
+    name: 'User Management',
+    tech: ['Java', 'SpringBoot', 'Thymeleaf', 'PostgreSQL', 'TailwindCSS'],
+    caption: 'Web App with Thymeleaf UI',
+    desc: 'Spring Boot app with UI built using Thymeleaf templates. PBKDF2 password encryption is used for authentication.',
     github: gitHub.thymeleaf,
-    youtube:youTube.thymeleaf,
-    image:'thymeleaf.svg'
-  },{
-    name:'E-Commerce',
-    tech:['Python', 'Django', 'TailwindCSS'],
-    caption:'eCommerce with Django',
-    desc:'Online Market Place built using Django, a web framework based on Python. It has sign up/login feature, messaging and other features.',
+    youtube: youTube.thymeleaf,
+    image: 'thymeleaf.svg',
+  },
+  {
+    name: 'E-Commerce',
+    tech: ['Python', 'Django', 'TailwindCSS'],
+    caption: 'eCommerce with Django',
+    desc: 'Online Market Place built using Django, a web framework based on Python. It has sign up/login feature, messaging and other features.',
     github: gitHub.python,
-    youtube:youTube.python,
-    image:'python.svg'
-  },{
-    name:'Showcase',
-    tech:['Angular','TailwindCSS'],
-    caption:'Demo website',
-    desc:'A basic demo website for tour planning company, made using Angular',
-    github:gitHub.demo,
-    youtube:youTube.demo,
-    image:'website-demo.svg'
-  },{
-    name:'Tic-Tac-Toe & Bingo',
-    tech:['Java'],
-    caption:'GUI with Java Swing',
-    desc:'GUI based games developed with Java Swing, a GUI widget tool.',
+    youtube: youTube.python,
+    image: 'python.svg',
+  },
+  {
+    name: 'Showcase',
+    tech: ['Angular', 'TailwindCSS'],
+    caption: 'Demo website',
+    desc: 'A basic demo website for tour planning company, made using Angular',
+    github: gitHub.demo,
+    youtube: youTube.demo,
+    image: 'website-demo.svg',
+  },
+  {
+    name: 'Tic-Tac-Toe & Bingo',
+    tech: ['Java'],
+    caption: 'GUI with Java Swing',
+    desc: 'GUI based games developed with Java Swing, a GUI widget tool.',
     github: gitHub.swing,
-    youtube:youTube.swing,
-    image:'swing.svg'
-  },{
-    name:'Word Search program',
-    tech:['C++'],
+    youtube: youTube.swing,
+    image: 'swing.svg',
+  },
+  {
+    name: 'Word Search program',
+    tech: ['C++'],
     caption: 'Search for words in files',
-    desc:'C++ program to search for a specific word in all files in a directory. The program searches only for the exact word, even if its surrounded by punctuations.',
+    desc: 'C++ program to search for a specific word in all files in a directory. The program searches only for the exact word, even if its surrounded by punctuations.',
     github: gitHub.c,
-    image:'c.svg'
+    image: 'c.svg',
   },
 ];
 export const about = {
-  bio:'Hi there, I am Arvindh. A full stack web developer who specialises in aNGular, Node, NestJS and Java.',
-  experience:'Yes, a 22 year old fresher who graduated college a year back.',
-  education:{course:'B.Tech, textile technology',year:'2022',percentage:'83%'},
-  join:'Available to start immediately.'
-}
+  bio1: 'Hi there, I am Arvindh. A full stack web developer who specialises in aNGular, Node, NestJS and Java.',
+  bio: 'Dynamic full-stack developer with a proven track record of continuous growth and a strong passion for technology. Seeking new challenges and opportunities to leverage skills and drive innovation in a collaborative team environment.',
+  experience1: 'Yes, a 22 year old fresher who graduated college a year back.',
+  experience:
+    'Currently, working as Full Stack Developer in Cholamandalam Investment and Finance Company Ltd',
+  education: {
+    course: 'B.Tech, textile technology',
+    year: '2022',
+    percentage: '83%',
+  },
+  join: 'My notice period is 30 days',
+};
